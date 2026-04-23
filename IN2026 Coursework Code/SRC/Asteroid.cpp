914987348ec703625b6e66b0c7376b0eb439e8fb
Gyryk <68961075+Gyryk@users.noreply.github.com>
2026-04-23 16:59:55 +0100

    Add asteroid types and collision

--- IN2026 Coursework Code/SRC/Asteroid.cpp
+++ IN2026 Coursework Code/SRC/Asteroid.cpp
@@ -2,8 +2,16 @@
 #include "GameUtil.h"
 #include "Asteroid.h"
 #include "BoundingShape.h"
+#include "Bullet.h"
+#include "Spaceship.h"
 
-Asteroid::Asteroid(void) : GameObject("Asteroid")
+namespace
+{
+	const float LARGE_BOUNCINESS = 0.9f;
+	const float SMALL_BOUNCINESS = 1.0f;
+}
+
+Asteroid::Asteroid(void) : GameObject("Asteroid"), mSize(LARGE), mDestroyedByBullet(false)
 {
 	mAngle = rand() % 360;
 	mRotation = 0; // rand() % 90;
@@ -15,13 +23,27 @@ Asteroid::Asteroid(void) : GameObject("Asteroid")
 	mVelocity.z = 0.0;
 }
 
+Asteroid::Asteroid(AsteroidSize size) : GameObject("Asteroid"), mSize(size), mDestroyedByBullet(false)
+{
+	mAngle = rand() % 360;
+	mRotation = 0;
+	mPosition.x = rand() / 2;
+	mPosition.y = rand() / 2;
+	mPosition.z = 0.0;
+	float base_speed = (mSize == LARGE) ? 10.0f : 16.0f;
+	mVelocity.x = base_speed * cos(DEG2RAD * mAngle);
+	mVelocity.y = base_speed * sin(DEG2RAD * mAngle);
+	mVelocity.z = 0.0;
+}
+
 Asteroid::~Asteroid(void)
 {
 }
 
 bool Asteroid::CollisionTest(shared_ptr<GameObject> o)
 {
-	if (GetType() == o->GetType()) return false;
+	if (o->GetType() == GameObjectType("Explosion")) return false;
+	if (o->GetType() == GameObjectType("PowerUp")) return false;
 	if (mBoundingShape.get() == NULL) return false;
 	if (o->GetBoundingShape().get() == NULL) return false;
 	return mBoundingShape->CollisionTest(o->GetBoundingShape());
@@ -29,5 +51,64 @@ bool Asteroid::CollisionTest(shared_ptr<GameObject> o)
 
 void Asteroid::OnCollision(const GameObjectList& objects)
 {
-	mWorld->FlagForRemoval(GetThisPtr());
+	for (GameObjectList::const_iterator it = objects.begin(); it != objects.end(); ++it)
+	{
+		shared_ptr<GameObject> other = *it;
+		if (other->GetType() == GameObjectType("Asteroid"))
+		{
+			// This prevents double processing
+			// if (GetThisPtr().get() < other.get())
+			// {
+				Bounce(other);
+			// }
+		}
+		else if (other->GetType() == GameObjectType("Bullet"))
+		{
+			mDestroyedByBullet = true;
+			mWorld->FlagForRemoval(GetThisPtr());
+			return;
+		}
+		else if (other->GetType() == GameObjectType("Spaceship"))
+		{
+			if (mSize == LARGE)
+			{
+				mWorld->FlagForRemoval(GetThisPtr());
+				return;
+			}
+			Bounce(other);
+		}
+	}
+}
+
+int Asteroid::GetScoreValue() const
+{
+	return (mSize == LARGE) ? 20 : 50;
+}
+
+void Asteroid::Bounce(shared_ptr<GameObject> o)
+{
+	GLVector3f delta = mPosition - o->GetPosition();
+	delta.z = 0.0f;
+	if (delta.length() == 0) delta = GLVector3f(1.0f, 0.0f, 0.0f);
+	delta.normalize();
+
+	GLVector3f this_velocity = mVelocity;
+	GLVector3f other_velocity = o->GetVelocity();
+
+	GLfloat this_normal_speed = this_velocity.dot(delta);
+	GLfloat other_normal_speed = other_velocity.dot(delta);
+
+	GLVector3f this_normal = delta * this_normal_speed;
+	GLVector3f other_normal = delta * other_normal_speed;
+	GLVector3f this_tangent = this_velocity - this_normal;
+	GLVector3f other_tangent = other_velocity - other_normal;
+
+	const float BOUNCINESS = (mSize == LARGE) ? LARGE_BOUNCINESS : SMALL_BOUNCINESS;
+	GLVector3f this_new_velocity = (other_normal + this_tangent) * BOUNCINESS;
+	GLVector3f other_new_velocity = (this_normal + other_tangent) * BOUNCINESS;
+
+	mPosition += delta * 0.2f;
+	o->SetPosition(o->GetPosition() - delta * 0.2f);
+	mVelocity = this_new_velocity;
+	o->SetVelocity(other_new_velocity);
 }
